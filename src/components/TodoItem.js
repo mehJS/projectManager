559d99e89171a657c7121083f8ea2f94bf47637b
Matti Hämäläinen <matti.hamalainen@valamis.com>
2019-04-02 18:38:01 +0300

    React project files, adding styles at some point

---
+++ src/components/TodoItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+
+  render() {
+	  // console.log(this.props);
+    return (
+      <li className="Todo">
+        <strong>{this.props.todo.title}</strong> 
+      </li>
+    );
+  }
+}
+
+
+export default TodoItem;
