53a7ab98c190b7398dba6aea0c15eda8b55079cb
Matti Hämäläinen <matti.hamalainen@valamis.com>
2019-04-02 18:44:51 +0300

    Reformatting and deleting unused files

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 
 class ProjectItem extends Component {
-  deleteProject(id){
-    this.props.onDelete(id);
-  }
+	deleteProject(id) {
+		this.props.onDelete(id);
+	}
 
-  render() {
-	  // console.log(this.props);
-    return (
-      <li className="Project">
-        <strong>{this.props.project.title}</strong> - {this.props.project.category} <a href ="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>
-      </li>
-    );
-  }
+	render() {
+		// console.log(this.props);
+		return (
+			<li className="Project">
+				<strong>{this.props.project.title}</strong> -{' '}
+				{this.props.project.category}{' '}
+				<a
+					href="#"
+					onClick={this.deleteProject.bind(this, this.props.project.id)}
+				>
+					X
+				</a>
+			</li>
+		);
+	}
 }
-
 
 export default ProjectItem;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component {
-
-  render() {
-	  // console.log(this.props);
-    return (
-      <li className="Todo">
-        <strong>{this.props.todo.title}</strong> 
-      </li>
-    );
-  }
+	render() {
+		// console.log(this.props);
+		return (
+			<li className="Todo">
+				<strong>{this.props.todo.title}</strong>
+			</li>
+		);
+	}
 }
-
 
 export default TodoItem;
